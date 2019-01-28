7c6ad9ee6a589e1bb7757b900d14832933cb2a51
RockyHsiao <rockyhsiao1016@gmail.com>
2019-01-28 11:23:53 -0800

    Insert the OANDA API data into DB (beta 1.5)

--- app/Http/Controllers/Frontend/TestController.php
+++ app/Http/Controllers/Frontend/TestController.php
@@ -31,41 +31,56 @@ class TestController extends Controller
         $result = json_decode($result);
 
 
-        $output = $this->getCandles($result); // b
+        $output = $this->getCandles($result);
 
-//        // Count the number of data from FX Street API
-//        $amount =  count($result->{'candles'});
-//
-//        for($i = 0;$i < $amount;$i++)
-//        {
-//            $time = $result->{'candles'}[$i]->{'time'};
-//            $Filter_text = array("T",".000000000Z");
-//            $time = str_replace($Filter_text,' ',$time);
-//            $open = $result->{'candles'}[$i]->{'mid'}->{'o'};
-//            $high = $result->{'candles'}[$i]->{'mid'}->{'h'};
-//            $low = $result->{'candles'}[$i]->{'mid'}->{'l'};
-//            $close = $result->{'candles'}[$i]->{'mid'}->{'c'};
-//
-//            echo "Time: ".$time."<br>";
-//            echo "Open price: ".$open."<br>";
-//            echo "Highest price: ".$high."<br>";
-//            echo "Lowest price: ".$low."<br>";
-//            echo "Close price: ".$close."<br>";
-//            echo "<br>";
-//        }
-
-        foreach($output as $FirstValue)
+        foreach($output as $APIData)
         {
-            Chart::API_data($FirstValue[0],$FirstValue[1],$FirstValue[2],$FirstValue[3],$FirstValue[4]);
+            Chart::API_data($APIData[0],$APIData[1],$APIData[2],$APIData[3],$APIData[4]);
         }
     }
 
+    public function getapi()
+    {
+        $token = env('OANDA_API_KEY');
+        $client = new Client(['base_uri' => 'https://api-fxpractice.oanda.com/']);
+        $headers = [
+            'Authorization' => 'Bearer '. $token,
+            'accountid' => env('OANDA_ACCOUNT_ID'),
+        ];
+        $response = $client->request('GET', 'v3/instruments/USD_CAD/candles', [
+            'headers' => $headers
+        ]);
+
+        $result = $response->getBody();
+        echo $result;
+        $result = json_decode($result);
+
+
+        $output = $this->getCandles($result);
+
+        $data = [];
+        foreach($output as $APIData)
+        {
+            $data[] =
+            [
+                $APIData[0],
+                $APIData[1],
+                $APIData[2],
+                $APIData[3],
+                $APIData[4]
+            ];
+        }
+        return $data;
+    }
+
+    // Preproccing API data
     public function getCandles($result)
     {
         $output = [];
 
         foreach($result->candles as $candle)
         {
+            // Filter some uselesss text from time
             $Filter_text = array("T",".000000000Z");
             $mid = $candle->mid;
 
--- routes/frontend/test.php
+++ routes/frontend/test.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\Frontend\TestController;
 
 Route::get('test', [TestController::class, 'index'])->name('test');
-
+Route::get('test/getapi', [TestController::class, 'getapi'])->name('getapi');
