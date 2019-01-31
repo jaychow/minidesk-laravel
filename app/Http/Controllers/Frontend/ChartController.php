646c93c2ceb58f3c91c100a43e4fd78dcb2ec4f8
RockyHsiao <rockyhsiao1016@gmail.com>
2019-01-31 11:19:50 -0800

    anychart 1 yeay data testing

--- app/Http/Controllers/Frontend/ChartController.php
+++ app/Http/Controllers/Frontend/ChartController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers\Frontend;
 use App\Models\Chart;
 use App\Http\Controllers\Controller;
@@ -7,22 +6,21 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Psr7;
 use GuzzleHttp\Exception\RequestException;
 use Illuminate\Http\Request;
-
 date_default_timezone_set('Europe/London'); // UTC + 0
-
-/* Class ChartController
+/**
+ * Class ChartController
  * @package App\Http\Controllers\Frontend
-*/
+ */
 class ChartController extends Controller
 {
-/* @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-*/
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         return view('frontend.chart');
     }
-
-    public function getAPI($type,$utc)
+    public function getAPI($type,$utc,$fromTime,$toTime)
     {
         $token = env('OANDA_API_KEY');
         $client = new Client(['base_uri' => 'https://api-fxpractice.oanda.com/']);
@@ -32,7 +30,7 @@ class ChartController extends Controller
         ];
         try
         {
-            $response = $client->request('GET', 'v3/instruments/'.$type.'/candles?granularity=M10&count=1000', [
+            $response = $client->request('GET', 'v3/instruments/'.$type.'/candles?from='.$fromTime.'&to='.$toTime.'&granularity=D', [
                 'headers' => $headers
             ]);
         }
@@ -40,22 +38,22 @@ class ChartController extends Controller
         {
 //            echo Psr7\str($e->getRequest());
 //            echo Psr7\str($e->getResponse());
-            echo "Can not get this API data";
+            echo "Can not get this API data"."<br>";
+            echo $fromTime."<br>";
+            echo $toTime."<br>";
             exit;
         }
-
         $result = $response->getBody();
         $result = json_decode($result);
         $output = $this->getCandles($result);
         $this->storeAPI($output);
-
         // Give frontend data with time calibration
         $final = [];
         foreach ($output as $data)
         {
             $final[]=
                 [
-                    $time = date("Y-m-d H:i:s",strtotime($data[0].''.$utc.' hour')),
+                    $time = date("Y-m-d",strtotime($data[0].''.$utc.' hour')),
                     $data[1],
                     $data[2],
                     $data[3],
@@ -64,10 +62,8 @@ class ChartController extends Controller
                     $data[6],
                 ];
         }
-
         echo json_encode($final);
     }
-
     // Preproccing API data
     public function getCandles($result)
     {
@@ -78,7 +74,6 @@ class ChartController extends Controller
             // Filter some uselesss text from time
             $Filter_text = array("T",".000000000Z");
             $mid = $candle->mid;
-
             $output[] =
                 [
                     $time = str_replace($Filter_text,' ',$candle->time),
@@ -92,40 +87,33 @@ class ChartController extends Controller
         }
         return array_reverse($output);
     }
-
     // Store API data into the DB
     public function storeAPI($data)
     {
         $query = [];
         $fromType = "";
         $toType = "";
-
         // Get API start time and end time
         $toTime = $data[0][0];
         $temp = array_reverse($data);
         $fromTime = $temp[0][0];
-
         foreach ($data as $value)
         {
             $type = explode('_', $value[1]);
             $from = $type[0];
             $to = $type[1];
-
             $fromType = $from.'_'.$to;
             $toType = $to.'_'.$from;
             $time = $value[0];
             $volume = $value[6];
-
             $fromOpen = $value[2];
             $fromHigh = $value[3];
             $fromLow = $value[4];
             $fromClose = $value[5];
-
             $toOpen = 1/$fromOpen;
             $toHigh = 1/$fromHigh;
             $toLow = 1/$fromLow;
             $toClose = 1/$fromClose;
-
             $query[] =
                 [
                     'time' => $time,
@@ -150,24 +138,32 @@ class ChartController extends Controller
         $this->fliterData($query,$fromTime,$toTime,$fromType,$toType);
         Chart::insert($query);
     }
-
     // Filter duplicated data in DB
     public function fliterData($query,$fromTime,$toTime,$fromType,$toType)
     {
         Chart::where('type',$fromType)->orwhere('type',$toType)->whereBetween('time',array($fromTime,$toTime))->delete();
     }
-
     // Response frontend request
-    public function getTable()  //Request $request
+    public function getTable(Request $request)  //Request $request
     {
-        $type = 'USD_CAD';
-        $utc = -8;
-//        $type = $request->get('pair');
-//        $utc = $request->get('utc');
+        //$type = 'USD_CAD';
+        $utc = -8;      // San Dirgo
+        $fromTime = date("Y-m-d",strtotime('-1 year'));
+        $toTime = date("Y-m-d");
+        $timeRange = "";
+        $type = $request->get('pair');
+//      $utc = $request->get('utc');
+        $timeRange = $request->get('timeRange');
+//      $fromTime = $request->get('from');
+//      $toTime = $request->get('to');
 
+        if($timeRange != "1Y")
+        {
+            exit;
+        }
         // Time interval
         $time2 = date("Y-m-d H:i:s");
-        $time1 = date("Y-m-d H:i:s",strtotime('-10 minute'));
+        $time1 = date("Y-m-d H:i:s",strtotime('-1 day'));
 
         // Check API data is exist or not ?
         $exist = Chart::where('type',$type)->whereBetween('time',array($time1,$time2))->exists();
@@ -180,7 +176,7 @@ class ChartController extends Controller
             {
                 $output[]=
                     [
-                        $time = date("Y-m-d H:i:s",strtotime($data->time.''.$utc.' hour')),
+                        $time = date("Y-m-d",strtotime($data->time.''.$utc.' hour')),
                         $data->type,
                         $data->open,
                         $data->high,
@@ -193,7 +189,7 @@ class ChartController extends Controller
         }
         else
         {
-            $this->getAPI($type,$utc);
+            $this->getAPI($type,$utc,$fromTime,$toTime);
         }
     }
 }
