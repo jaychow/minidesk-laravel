242323f64f382620c7d4474971cf566b099bb4f3
Jay <jay@coretekllc.com>
2019-02-20 19:27:18 -0800

    added chartzone admin breadcrumb

--- resources/lang/en/strings.php
+++ resources/lang/en/strings.php
@@ -29,6 +29,10 @@ return [
             'welcome' => 'Welcome',
         ],
 
+        'chartzone' => [
+            'title' => 'Chart Zone'
+        ],
+
         'general' => [
             'all_rights_reserved' => 'All Rights Reserved.',
             'are_you_sure'        => 'Are you sure you want to do this?',
--- routes/breadcrumbs/backend/backend.php
+++ routes/breadcrumbs/backend/backend.php
@@ -4,5 +4,9 @@ Breadcrumbs::for('admin.dashboard', function ($trail) {
     $trail->push(__('strings.backend.dashboard.title'), route('admin.dashboard'));
 });
 
+Breadcrumbs::for('admin.chartzone', function ($trail) {
+    $trail->push('strings.backend.chartzone.title', route('admin.chartzone'));
+});
+
 require __DIR__.'/auth.php';
 require __DIR__.'/log-viewer.php';
