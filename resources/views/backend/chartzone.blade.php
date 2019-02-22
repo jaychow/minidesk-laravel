2f3a8cbe4c60264d5db67995a1418c8c221d830c
bombobutt <bombobutt@gmail.com>
2019-02-21 22:12:32 -0800

    【Backend/chartzone】revise zone.js

--- resources/views/backend/chartzone.blade.php
+++ resources/views/backend/chartzone.blade.php
@@ -4,42 +4,39 @@
 
 @section('content')
     <div id="whole">
-        <div id="chart-container">
-            <div id="chart"></div>
-        </div>
 
         <div id="panel-container">
             <div id="form-container">
-                <form id="zoneForm">
-                    <select name="pair" id="pairOptions">
-                        <option disabled selected value> -- pairs -- </option>
-                        <option value="GBP_USD">GBP_USD</option>
-                        <option value="USD_GBP">USD_GBP</option>
-                        <option value="USD_CAD">USD_CAD</option>
-                        <option value="CAD_USD">CAD_USD</option>
-                        <option value="EUR_USD">EUR_USD</option>
-                        <option value="USD_EUR">USD_EUR</option>
-                    </select> <br/>
+                <select name="pair" id="pairOptions">
+                    <option disabled selected value> -- pairs -- </option>
+                    <option value="GBP_USD">GBP_USD</option>
+                    <option value="USD_GBP">USD_GBP</option>
+                    <option value="USD_CAD">USD_CAD</option>
+                    <option value="CAD_USD">CAD_USD</option>
+                    <option value="EUR_USD">EUR_USD</option>
+                    <option value="USD_EUR">USD_EUR</option>
+                </select>
 
-                    <input name="fromDate" id="fromDate" type="date" max="<?php echo date("Y-m-d"); ?>"> <br/>
-
-                </form>
-
-                <button id="refreshButton" class="panelButton">refresh</button> <br/>
-
-            </div> <br/> <hr> <br/>
+            </div> <hr> <br/>
 
             <div id="zoneInfo">
                 <h4> - Zones - </h4> <br/>
                 <table id="zoneTable">
-                    <tr>
+                    <thead>
                         <td>#</td>
                         <td>Start date</td>
                         <td>High price</td>
                         <td>Low price</td>
-                        <td>Remove</td>
-                    </tr>
-                </table>
+                        <td>Clear Column</td>
+                    </thead>
+                    <tbody id="zoneTableBody">
+
+                    </tbody>
+                </table> <br/>
+
+                @csrf
+                <button id="submitButton">Submit</button>
+
             </div>
         </div>
     </div>
