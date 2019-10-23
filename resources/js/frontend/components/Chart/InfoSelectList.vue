35be0c3306c0496f5038e7d304019fbb946f17b1
林昱明 <mitlab@122-ip148.et.ntust.edu.tw>
2019-10-23 16:43:41 +0800

    add minichart view

--- resources/js/frontend/components/Chart/InfoSelectList.vue
+++ resources/js/frontend/components/Chart/InfoSelectList.vue
@@ -9,7 +9,7 @@
                     <div class="unit">{{ infoPagesData.descriptList[currency]["unit"]}}</div>
                 </div>
             </div>
-            <div class="mini-chart"> MINICHART </div>
+            <div v-bind:id="minichartClass(i)" class="minichart" ref="minichart"></div>
             <div class="now-price-area">
                 <div class="now-price-button">
                     <button class="submitButton" id="info-select-button"> {{ formatPrice(infoPagesData.infoPagesPriceData[infoPagesData.infoPair[i]][0][5]) }} </button>
@@ -32,28 +32,69 @@ export default {
     name: "info-select-list",
     data(){
         return{
-            formatPrice(price, digit=4){
-                price = parseFloat(price)
-                return price.toFixed(digit)
-            },
-            formatChange(priceChange, priceRange, changeDigit=3, rangeDigit=3){
-                var change = parseFloat(priceChange).toFixed(changeDigit)
-                var range = parseFloat(priceRange).toFixed(rangeDigit)
-                var s = ""
-                if(change >= 0)
-                    s = "+" + change.toString() + "%"
-                else
-                    s = change.toString() + "%"
-                s += " " + range.toString()
-                return s
-            },
+            
         }
     },
-    method:{
-        
+    methods:{
+        formatPrice(price, digit=4){
+            price = parseFloat(price)
+            return price.toFixed(digit)
+        },
+        formatChange(priceChange, priceRange, changeDigit=3, rangeDigit=3){
+            var change = parseFloat(priceChange).toFixed(changeDigit)
+            var range = parseFloat(priceRange).toFixed(rangeDigit)
+            var s = ""
+            if(change >= 0)
+                s = "+" + change.toString() + "%"
+            else
+                s = change.toString() + "%"
+            s += " " + range.toString()
+            return s
+        },
+        minichartClass(index){
+            var c = "minichart_" + index
+            return c
+        },
+        draw(){
+            this.$nextTick(function(){
+                for(var i=0;i<this.infoPagesData.selectList.length; i++){
+                    console.log(this.$refs.minichart[i])
+                    console.log("draw " + i)
+                    var historyDataTable = Anychart.data.table();  
+                    var chart = Anychart.stock();
+                    chart.tooltip(false);
+                    chart.scroller().enabled(false);
+                    chart.bounds( "-50%", "-10%", '150%', '100%');
+                    chart.crosshair(false);
+                    var historyPlot = chart.plot(0);
+                    historyPlot.xAxis(false);
+                    historyPlot.yAxis(false);
+                    historyPlot.height('100%')
+                        .width('100%');
+
+                    historyPlot.legend(false);
+                    
+                    var line_mapping = historyDataTable.mapAs({
+                        'value': 5
+                    });
+                    const line_series = historyPlot.line(line_mapping);
+                    line_series.id("line");
+                    line_series.stroke("1 #8b8dbb");
+                    historyDataTable.addData(this.infoPagesData.infoPagesPriceData[this.infoPagesData.infoPair[i]])
+                    chart.container("minichart_"+i)
+
+                    chart.draw()
+                }
+            })
+        },
     },
     computed: {
         ...mapGetters(['infoPagesData'])
+    },
+    watch:{
+        infoPagesData: function(){
+            this.draw()
+        }
     },
 }
 </script>
